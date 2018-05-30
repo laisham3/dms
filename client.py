f927648ed8e5342befef4672c95bd79e252c8526
laisham3 <31894574+laisham3@users.noreply.github.com>
2018-05-30 11:07:07 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,31 @@
-class Chat():
-  def accept_connections():
-    client, client_address = SERVER.accept()
-    print("other has connected." % client_address)
-    client.send(bytes("Welcome to dms!"+
-                        "Start by typing your name and pressing enter!"))
-    addresses[client] = client_address
-  
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+        except OSError:
+            break
+         
+def send(event=None):
+    msg = my_msg.get()
+    my_msg.set("")  
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        top.quit()
+        
+def closing(event=None):
+    my_msg.set("{quit}")
+    send()
+    
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
