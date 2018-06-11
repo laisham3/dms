f1530e52b3f8ffc91c63a9ae81df52660faf2c06
laisham3 <31894574+laisham3@users.noreply.github.com>
2018-06-10 21:16:33 -0400

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ def accept_incoming_connections():
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        print("Welcome to dms!")input('Type your name and press enter: ')
+        print("Welcome to dms!")
         client.send(bytes("Welcome to dms! Type your name and press enter to start!", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
